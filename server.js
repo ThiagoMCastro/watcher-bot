2a176f6e2d17006106fe8feb686e85cdc352042f
Glitch (hello-express) <none>
2019-01-23 16:46:40 +0000

    🛩🚔 Checkpoint ./server.js:2510383/16245 ./package.json:2510383/1473 ./a:2510383/1896 ./config.json:2510383/218

--- server.js
+++ server.js
@@ -1,22 +1,62 @@
-// server.js
-// where your node app starts
-
-// init project
+const http = require('http');
 const express = require('express');
 const app = express();
-
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.get("/", (request, response) => {
+  console.log(Date.now() + "Ping Received");
+  response.sendStatus(200);
 });
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
+const fs = require('fs')
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+const config = require("./config.json");
+const client = new Discord.Client();
+bot.commands = new Discord.Collection();
+
+
+
+fs.readdir('./commands/', (err, files) => {
+
+  if(err) console.log(err);
+
+  let jsfile = files.filter(f => f.split(".").pop() === "js")
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands")
+    return;
+  }
+    setTimeout(function(){ 
+    console.log("Beginning startup"); 
+}, 0);
+  
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/${f}`)
+    console.log(`${f} loaded`);
+    bot.commands.set(props.help.name, props);
+  });
+
+    setTimeout(function(){ 
+    console.log("Almost ready!"); 
+}, 1000);
+  setTimeout(function(){ 
+    console.log("I'm ready! Now logging commands"); 
+}, 1500);
 });
+bot.on("message", async message => {
+  
+ 
+
+  let prefix = config.prefix;
+  let messageArray = message.content.split(" ");
+  let command = messageArray[0];
+  let args = messageArray.slice(1);
+  let commandfile = bot.commands.get(command.slice(prefix.length));
+  if (!message.content.startsWith(prefix)) return;
+  if(commandfile) commandfile.run(bot,message,args);
+  var author = message.author;
+})
+
+bot.login(config.token);
