7513c5c367470f2c128547cd1bcf0a1f89d3b036
Glitch (hello-express) <none>
2020-01-16 07:54:27 +0000

    🛍😎 Checkpoint ./commands/limpar.js:3915775/1690

--- commands/limpar.js
+++ commands/limpar.js
@@ -1,32 +1,13 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    if (!message.channel.permissionsFor(message.author).hasPermission("MANAGE_MESSAGES")) {
-        message.channel.sendMessage("Perdão meu amigo, você não tem nenhuma permissao para fazer isso!\""+message.content+"\"");
-        console.log("Voce não tem permissao para fazer isso\""+message.content+"\"");
-        return;
-      } else if (!message.channel.permissionsFor(bot.user).hasPermission("MANAGE_MESSAGES")) {
-        message.channel.sendMessage("Desculpe-me, eu não tenho permissao.\""+message.content+"\"");
-        console.log("Eu não tenho permissao \""+message.content+"\"");
-        return;
-      }
-
-      // Only delete messages if the channel type is TextChannel
-      // DO NOT delete messages in DM Channel or Group DM Channel
-      if (message.channel.type == 'text') {
-        message.channel.fetchMessages()
-          .then(messages => {
-            message.channel.bulkDelete(messages);
-
-            // Logging the number of messages deleted on both the channel and console.
-            message.channel.sendMessage("Mensagens deletadas com sucesso.");
-            console.log('Mensagens deletadas com sucesso.')
-          })
-          .catch(err => {
-            console.log('Erro durante a deletação das mensagens.');
-            console.log(err);
-          });
-      }
+  var adr = parseInt(args[0], 10);
+    if (message.member.hasPermission("MANAGE_MESSAGES")) {
+            message.channel.fetchMessages()
+               .then(function(list){
+                    message.channel.bulkDelete(adr);
+                }, function(err){message.channel.send("Erro ao deletar")})                        
+        }
 }
 
 module.exports.help = {
