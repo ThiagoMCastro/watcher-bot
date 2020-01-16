2df1e299ba3ac0e49085aea18fc86534bdd170d3
Glitch (hello-express) <none>
2020-01-16 02:57:28 +0000

    🎷👋 Checkpoint ./commands/busca.js:3915775/58 ./commands/pedir.js:3915775/565

--- commands/pedir.js
+++ commands/pedir.js
@@ -1,10 +1,10 @@
 const Discord = require("discord.js");
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'johnny.heliohost.org',
-  user     : 'areaseca',
+  host     : 's1.cpanel.ecxon.com.br',
+  user     : 'watchert_thiago',
   password : 'Thiagom0@',
-  database : 'areaseca_watcher'
+  database : 'watchert_db'
 });
 module.exports.run = async (bot, message, args) => {
 connection.connect();
@@ -12,6 +12,7 @@ var adr = args.splice(0,100).join(" ")
 connection.query(`INSERT INTO pedidos (id, pedinte, filme) VALUES (NULL, '${message.author.username}',` + mysql.escape(adr) + `)`, function (err) {
   if (err) throw err;
   message.channel.send(`Ok, ${message.author}. Pedido feito! Seu pedido foi ` + (adr));
+  bot.channels.get(`665976048587898920`).send(`${message.author} Fez um pedido, no caso foi:  ` + (adr))
 });
  
 connection.end();
