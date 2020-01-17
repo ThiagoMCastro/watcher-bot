4f0b863c92ff01ccda4f1c5bd69f15582eda36dd
Glitch (hello-express) <none>
2020-01-17 01:46:41 +0000

    📟🐚 Checkpoint ./commands/dinheiro.js:3915775/4008

--- commands/dinheiro.js
+++ commands/dinheiro.js
@@ -1,38 +1,48 @@
 const Discord = require("discord.js");
-var mysql      = require('mysql');
-var utf8 = require('utf8');
+var mysql = require("mysql");
+var utf8 = require("utf8");
 var connection = mysql.createConnection({
-  host     : 's1.cpanel.ecxon.com.br',
-  user     : 'watchert_thiago',
-  password : 'Thiagom0@',
-  database : 'watchert_db'
+  host: "s1.cpanel.ecxon.com.br",
+  user: "watchert_thiago",
+  password: "Thiagom0@",
+  database: "watchert_db"
 });
 module.exports.run = async (bot, message, args) => {
-connection.connect();
-var sql = 'SELECT * FROM discord WHERE idd = ' + `${message.author.id}`;
-connection.query(sql, function (err, rows) {
-  if(err) {
-        conDB.end();
+  connection.connect();
+  connection.query(
+    `SELECT * FROM discord WHERE idd = ${message.author.id}`,
+    function(err, rows) {
+      if (err) {
         return console.log(err);
-    }
+      }
 
-    if (!rows.length)
-    {
-        connection.query('INSERT INTO Usuario SET ?',reg,function(err, results){
-            connection.end();
-            me
-        });
+      if (!rows.length) {
+        connection.query(
+          "INSERT INTO `discord` (`idd`, `usuario`, `dinheiro`) VALUES ('" +
+            `${message.author.id}` +
+            "','" +
+            `${message.author.username}` +
+            "', '6')",
+          function(err, results) {
+            message.channel.send(
+              "Você nao tinha uma conta, e criei uma pra você. Tens 6 reais!"
+            );
+          }
+        );
+      } else {
+        connection.query(
+          "SELECT FROM discord WHERE idd = " + `${message.author.id}`,
+          function(err, results) {
+            message.channel.send("Ja tem uma conta :)");
+          }
+        );
+      }
     }
-    else
-    {
-        conDB.end();
-        return res.send("Este usuario ya existe");
-    }
-});
- 
-connection.end();
-}
+  );
+
+  connection.end();
+};
 
 module.exports.help = {
   name: "saldo"
-}
+};
